ff6115e952b8d80789b159d90ba8510ba9ba8f79
Mariusz Piskor <opengranary@gmail.com>
2015-01-23 10:42:21 +0100

    added my solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions do (I hope) what the assignment requires - makeCacheMatrix caches the matrix and returns a list of functions, with the data "built in", and the cacheSolve function either solves for the inverse and stores (caches) the result in the object passed, or reads the cached data and returns it
+## 
 
-## Write a short comment describing this function
+## This function follows the example and the assignemnt description - it takes data (the default is an empty matrix) sets (function "set") the matrix one level above (<<-) - i.e. in the environment of makeCacheMatrix, defines three other helper functions (get, setinv, getinv) and returns the list of all the 4 functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(N){
+        M <<- N
+        inverse <<- NULL
+    }
+    get <- function() M
+    setinv <- function(inv) inverse <<- inv
+    getinv <- function() inverse
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This functions gets an object returned by makeCacheMatrix, which, among others, contains the matrix to be inverted, and if there is no inverted matrix in the object it calculates the result. If the inverse has already been calculated it returns the cached value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- MC$getinv()
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
+    data <- MC$get()
+    inverse <- solve(data, ...)
+    MC$setinv(inverse)
+    ## Return a matrix that is the inverse of 'x'
+    inverse
+    
 }
